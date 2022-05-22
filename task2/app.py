99ac013ba1dec1d01247c9bd1a11c5fd7c3df105
Lucky Horseshoe <lucky@ya.ru>
2022-05-22 11:33:33 +0300

    Add the possibility to add a new student to the table

--- task2/app.py
+++ task2/app.py
@@ -17,4 +17,31 @@ def index():
     return render_template('base.html', title='Students', students=students)
 @app.route('/create/', methods=('GET', 'POST'))
 def create():
+    if request.method == 'POST':
+        student_id = request.form['id']
+        name = request.form['name']
+        surname = request.form['surname']
+        patronym = request.form['patronym']
+        date = request.form['date']
+        group = request.form['group']
+
+        if not student_id:
+            flash('Id is required!')
+        elif not name:
+            flash('Name is required!')
+        elif not surname:
+            flash('Surname is required!')
+        elif not patronym:
+            flash('Patronym is required!')
+        elif not date:
+            flash('Date is required!')
+        elif not group:
+            flash('Group is required!')
+        else:
+            conn = get_db_connection()
+            conn.execute('INSERT INTO students VALUES (?, ?, ?, ?, ?, ?)',
+                         (student_id, name, surname, patronym, date, group))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
     return render_template('create.html')
