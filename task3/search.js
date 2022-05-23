62ae191d3c487a7baf03fd6b7b08dec72fe23ba1
Lucky Horseshoe <lucky@ya.ru>
2022-05-23 07:47:12 +0300

    Add search

---
+++ task3/search.js
@@ -0,0 +1,12 @@
+function Search(){
+	let name = document.getElementById('search').value;
+	var todo_names = document.getElementsByClassName('todo_name')
+	for (let i = 0; i < todo_names.length; i++){
+		if(todo_names[i].textContent != name){
+			todo_names[i].parentElement.style.display = 'none';
+		};
+	};
+};
+document.getElementById('search').onkeydown = (event) => {
+	if (event.code === 'Enter') { Search() };
+	};
